1527a3ef638758484d3ab06dbc07f45e4625af56
ted-orourke <torourke97@gmail.com>
2016-06-20 19:43:37 +0200

    Added cluster id results from basic clustering

--- Running Clusters.R
+++ Running Clusters.R
@@ -29,7 +29,7 @@ library(cluster)
 baseball.pam <- pam(cluster.data.1, 6)
 
 # Cluster pitches data on characteristics (without location)
-cluster.data.2 <- pitches %>% select(start_speed, end_speed, pfx_x, pfx_z, vx0, vy0, vz0, break_y, break_angle, break_length, spin_dir, spin_rate)
+cluster.data.2 <- pitches.clean %>% select(start_speed, end_speed, pfx_x, pfx_z, vx0, vy0, vz0, break_y, break_angle, break_length, spin_dir, spin_rate)
 cluster.data.2$break_y <- as.numeric(cluster.data.2$break_y)
 cluster.data.2$break_angle <- as.numeric(cluster.data.2$break_angle)
 cluster.data.2$break_length <- as.numeric(cluster.data.2$break_length)
@@ -39,3 +39,17 @@ ratios.2 = sapply(1:10, function(k) {
   with(kmeans(cluster.data.2, k, nstart = 10), betweenss / totss)
 })
 plot(ratios.2 ~ c(1:10))
+
+
+# Extract cluster id's for each pitch
+library(cluster)
+baseball.pam <- pam(cluster.data.2, 3)
+pitches.clean$cluster_id <- baseball.pam$clustering
+
+# pitches.clean has all the pitching data, as well as the cluster id's
+
+
+
+
+
+
