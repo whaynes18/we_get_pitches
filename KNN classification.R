6ac719eb5c9ccb334cb2a96bfe5caf6f63ee2974
ted-orourke <torourke97@gmail.com>
2016-06-21 15:19:00 +0200

    Cleaned up the file a little bit more

--- KNN classification.R
+++ KNN classification.R
@@ -4,12 +4,14 @@ library(magrittr)
 library(plyr)
 library(cluster)
 library(caret)
+library(kknn)
 
+# Load data (from pitches and the API)
 data("pitches")
-
 dat <- scrape(start = "2012-06-03", end = "2012-06-03")
 pitchFX <- plyr::join(dat$atbat, dat$pitch, by = c("num", "url"), type = "inner")
 
+# Clean data
 pitches.clean <- pitchFX
 pitches.clean$idNum <- sample(1:nrow(pitches.clean), nrow(pitches.clean))
 pitches.clean$break_y <- as.numeric(pitches.clean$break_y)
@@ -23,6 +25,7 @@ pitches.clean$des <- as.factor(pitches.clean$des)
 levels(pitches.clean$des) <- c("Ball", "Ball", "Called Strike", "Foul", "Foul", "Foul", "Foul", "Ball", "In play, no out", "In play, out", "In play, no out", "Swinging Strike", "Swinging Strike", "Swinging Strike")
 pitches.clean$des <- as.factor(pitches.clean$des)
 
+# Add Outcome Columns
 outcome <- pitches.clean  %>%  select(event, des, idNum)
 
 rows <- nrow(outcome)
@@ -36,17 +39,16 @@ for (i in 1:rows){
 }
 
 names(outcome) <- c("event", "des", "idNum", "end")
-
 pitches.outcomes <- merge(pitches.clean, outcome, by = "idNum")
 pitches.outcomes <- pitches.outcomes %>% filter(end != "Bunt Groundout", end != "Bunt Pop Out", end != "Double Play", end != "Field Error", end != "Sac Bunt")
 pitches.outcomes$end <- as.factor(pitches.outcomes$end)
 levels(pitches.outcomes$end) <- c("Ball", "Called Strike", "Double", "Groundout", "Flyout", "Groundout","Foul","Groundout","Groundout","Home Run","Lineout","Pop Out","Sac Fly","Single","Swinging Strike","Triple")
 pitches.outcomes$end <- as.factor(pitches.outcomes$end)
 
+# Select our relevant columns, split into train and test
 pitches.clean <- pitches.outcomes
 
 pitches.model.data <- pitches.clean %>% select(start_speed, end_speed, pfx_x, pfx_z, vx0, vy0, vz0, break_y, break_angle, break_length, spin_dir, spin_rate, zone, end)
-
 pitches.model.data <- na.omit(pitches.model.data)
 
 pitches.model.data[,1:12] <- scale(pitches.model.data[,1:12])
@@ -59,58 +61,25 @@ pitches.test <- pitches.model.data[-index,]
 pitches.train <- na.omit(pitches.train)
 pitches.test <- na.omit(pitches.test)
 
-# Use for kknn
-kknn.all <- kknn(pitches.train$end ~ ., train = filter(pitches.train)[-c(13,14)], test = pitches.test[-c(13,14)], k = 18)
+# Use knn to determine the optimal number of k-neighbors to choose (14 is our best accuracy)
+library(class)
+accuracy <- rep(0, 20)
+k <- 1:20
+for(x in k){
+  prediction <- knn(pitches.train[,-c(13,14)], pitches.test[,-c(13,14)], (pitches.train$end), k = x)
+  accuracy[x] <- mean(prediction == pitches.test$end)
+}
+plot(k, accuracy, type = 'b')
+
+# Use kknn to make a model
+kknn.all <- kknn(pitches.train$end ~ ., train = filter(pitches.train)[-c(13,14)], test = pitches.test[-c(13,14)], k = 14)
 kknn.all$prob
 
-# Use kknn for a zone
-kknn.zone.4 <- kknn(filter(pitches.train, zone == 4)$end ~ ., train = filter(pitches.train, zone == 4)[-c(13,14)], test = filter(pitches.test, zone ==4)[-c(13,14)], k = 18)
-kknn.zone.4$prob
-
-# Write a function to generate for a specific zone, pitch
+# Now write a function to generate barplot for a specific zone, pitch
 attempt.zone <- 2
 attempt.pitch <- pitches.clean[445,]
 
 the.big.guy <- function(zone, pitch) {
-  model <- kknn(filter(pitches.model.data, zone == zone_id)$end ~ ., train = filter(pitches.model.data, zone == zone_id)[-c(13,14)], test = pitch, k = 18)
+  model <- kknn(filter(pitches.model.data, zone == zone_id)$end ~ ., train = filter(pitches.model.data, zone == zone_id)[-c(13,14)], test = pitch, k = 14)
   barplot(model$prob)
-}
-
-## Old Method using kkn (no probabilites)
-# kth nearest neighbor on pitch outcomes
-pitches.model.data <- pitches.clean %>% select(start_speed, end_speed, pfx_x, pfx_z, vx0, vy0, vz0, break_y, break_angle, break_length, spin_dir, spin_rate, zone, des)
-
-pitches.model.data <- na.omit(pitches.model.data)
-
-pitches.model.data[,1:12] <- scale(pitches.model.data[,1:12])
-
-index <- createDataPartition(y = pitches.model.data$des, p=0.8)[[1]]
-pitches.train <- pitches.model.data[index,]
-pitches.test <- pitches.model.data[-index,]
-
-pitches.train <- na.omit(pitches.train)
-pitches.test <- na.omit(pitches.test)
-
-library(class)
-knn.1 <- knn(pitches.train[,-c(13,14)], pitches.test[,c(-13,-14)], (pitches.train$des), k = 11)
-
-# 18 is our best accuracy
-accuracy <- rep(0, 20)
-k <- 1:20
-for(x in k){
-  prediction <- knn(pitches.train[,-c(13,14)], pitches.test[,-c(13,14)], (pitches.train$des), k = x)
-  accuracy[x] <- mean(prediction == pitches.test$des)
-}
-
-plot(k, accuracy, type = 'b')
-
-knn.2 <- knn(pitches.train[,-c(13,14)], pitches.test[-c(13,14)], (pitches.train$des), k = 18)
-
-table(knn.2, pitches.test$des)
-barplot(prop.table(table(knn.2, pitches.test$des), 1)[1,])
-
-outcomes_list <- list()
-zones <- c(1:9, 11:14)
-for(x in zones) {
-  outcomes_list[[x]] <- knn(filter(pitches.train, zone == x)[-c(13,14)], filter(pitches.test, zone == x)[-c(13,14)], filter(pitches.train, zone == x)$des, k = 14)
 }
