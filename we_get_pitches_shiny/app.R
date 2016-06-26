4ff20fbc6eca3c18c1ccf331ea4bbad306465214
Lloyd Shatkin <lshatkin@umich.edu>
2016-06-26 17:49:31 +0200

    one big guy function

--- we_get_pitches_shiny/app.R
+++ we_get_pitches_shiny/app.R
@@ -647,27 +647,27 @@ server <- shinyServer(function(input, output, session) {
   ########################################################  
   
   
-  the.big.guy.R.pitcher <- function(zone_id, pitcher = "All", other_pitcher = "None") {
+  the.big.guy.pitcher <- function(zone_id, pitcher = "All", other_pitcher = "None", stance) {
     if (pitcher != "All") {
       if (other_pitcher != "None") {
         if (zone_id == 0){
           if (other_pitcher == "All"){
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == "R")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, stand == "R")
+            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == stance)
+            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, stand == stance)
           }
           else{
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "R")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, zone == zone_id, stand == "R")
+            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == stance)
+            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, zone == zone_id, stand == stance)
           }
         }
         else{
           if (other_pitcher == "All"){
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "R")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, zone == zone_id, stand == "R")
+            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == stance)
+            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, zone == zone_id, stand == stance)
           }
           else{
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "R")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, zone == zone_id, stand == "R")
+            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == stance)
+            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, zone == zone_id, stand == stance)
           }
         }
         
@@ -694,10 +694,10 @@ server <- shinyServer(function(input, output, session) {
       }
       else{
         if (zone_id == 0){
-          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == "R")
+          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == stance)
         }
         else{
-          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "R")
+          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == stance)
         }
         model <- kknn(relevant.data$end ~ ., train = relevant.data[-c(5:8)], test = test.pitch(), k = sqrt(nrow(relevant.data)))
         m2 <- data.frame(model$prob)
@@ -708,10 +708,10 @@ server <- shinyServer(function(input, output, session) {
     } 
     else {
       if (zone_id == 0){
-        relevant.data <- filter(pitches.model.data, stand == "R")
+        relevant.data <- filter(pitches.model.data, stand == stance)
       }
       else{
-        relevant.data <- filter(pitches.model.data, zone == zone_id, stand == "R")
+        relevant.data <- filter(pitches.model.data, zone == zone_id, stand == stance)
       }
       model <- kknn(relevant.data$end ~ ., train = relevant.data[-c(5:8)], test = test.pitch(), k = sqrt(nrow(relevant.data)))
       m2 <- data.frame(model$prob)
@@ -721,91 +721,17 @@ server <- shinyServer(function(input, output, session) {
     }
   }
   
-  the.big.guy.L.pitcher <- function(zone_id, pitcher = "All", other_pitcher = "None") {
-  
-    if (pitcher != "All") {
-      if (other_pitcher != "None") {
-        if (zone_id == 0){
-          if (other_pitcher == "All"){
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == "L")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, stand == "L")
-          }
-          else{
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == "L")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, stand == "L")
-          }
-        }
-        else{
-          if (other_pitcher == "All"){
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "L")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name != pitcher, zone == zone_id, stand == "L")
-          }
-          else{
-            relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "L")
-            relevant.data.2 <- filter(pitches.model.data, pitcher_name == other_pitcher, zone == zone_id, stand == "L")
-          }
-        }
-        
-        model.pitcher <- kknn(relevant.data$end ~ ., train = relevant.data[-c(5:8)], test = test.pitch(), k = sqrt(nrow(relevant.data)))
-        model.all <- kknn(relevant.data.2$end ~ ., train = relevant.data.2[-c(5:8)], test = test.pitch2(), k = sqrt(nrow(relevant.data.2)))
-        
-        pitcher.probs <- as.numeric(model.pitcher$prob)
-        all.probs <- as.numeric(model.all$prob)
-        outcomeLevels <- levels(pitches.clean$end)
-        both.probs <- data.frame(outcomeLevels, pitcher.probs, all.probs)
-        
-        outcomes <- melt(both.probs)
-        all <- outcomes %>% filter(variable == "all.probs")
-        selected.pitcher <- outcomes %>% filter(variable == "pitcher.probs")
-        
-        selected.pitcher$other <- selected.pitcher$outcomeLevels
-        all$other <- "League Average"
-        outcomes <- rbind(selected.pitcher, all)
-        
-        outcomes$outcomeLevels <- factor(all$outcomeLevels,levels(all$outcomeLevels)[c(10, 2, 4, 1, 5, 8, 3, 7, 9, 11, 6)])
-        selected.pitcher$outcomeLevels <- factor(selected.pitcher$outcomeLevels,levels(selected.pitcher$outcomeLevels)[c(10, 2, 4, 1, 5, 8, 3, 7, 9, 11, 6)])
-        
-        ggplot(outcomes, aes(x = outcomeLevels, y = value, fill = other)) + theme_bw() + theme(axis.text.x = element_text(angle = 30, hjust = 1), plot.title = element_text(size = 22, face = "bold"), axis.text = element_text(size = 13), axis.title = element_text(size = 19)) + guides(fill = FALSE) + geom_bar(colour = "black", stat = "identity", position = "dodge") + scale_fill_manual(values = c("yellow1", "green3", "blue3", "green3", "blue3", "red3", "blue3", "blue3", "red3", "green3", "red3", "grey75")) + ylab("Probability") + xlab("") + ggtitle("Comparison Pitch Outcome Distribution - Lefty Hitters")
-      }
-      else{
-        if (zone_id == 0){
-          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, stand == "L")
-        }
-        else{
-          relevant.data <- filter(pitches.model.data, pitcher_name == pitcher, zone == zone_id, stand == "L")
-        }
-        model <- kknn(relevant.data$end ~ ., train = relevant.data[-c(5:8)], test = test.pitch(), k = sqrt(nrow(relevant.data)))
-        m2 <- data.frame(model$prob)
-        outcomes <- melt(m2)
-        outcomes$variable <- factor(outcomes$variable,levels(outcomes$variable)[c(11, 2, 6,1, 4, 9, 5, 8, 10, 3, 7)])
-        ggplot(outcomes, aes(x = variable, y = value)) + theme_bw() + theme(axis.text.x = element_text(angle = 30, hjust = 1), plot.title = element_text(size = 22, face = "bold"),axis.text = element_text(size = 13), axis.title = element_text(size = 19)) + guides(fill = FALSE) + scale_fill_manual(values = c("green3", "green3", "green3","yellow1", "blue3", "blue3", "blue3", "blue3","red3", "red3", "red3")) + geom_bar(stat = "identity", colour = "black", aes(fill = variable)) + ylab("Probability") + xlab("") + ggtitle("Pitch Outcome Distribution - Lefty Hitters")
-      }
-    } 
-    else {
-      if (zone_id == 0){
-        relevant.data <- filter(pitches.model.data, stand == "L")
-      }
-      else{
-        relevant.data <- filter(pitches.model.data, zone == zone_id, stand == "L")
-      }
-      model <- kknn(relevant.data$end ~ ., train = relevant.data[-c(5:8)], test = test.pitch(), k = sqrt(nrow(relevant.data)))
-      m2 <- data.frame(model$prob)
-      outcomes <- melt(m2)
-      outcomes$variable <- factor(outcomes$variable,levels(outcomes$variable)[c(11, 2, 6,1, 4, 9, 5, 8, 10, 3, 7)])
-      ggplot(outcomes, aes(x = variable, y = value)) + theme_bw() + theme(axis.text.x = element_text(angle = 30, hjust = 1), plot.title = element_text(size = 22, face = "bold"), axis.text = element_text(size = 13), axis.title = element_text(size = 19)) + guides(fill = FALSE) + scale_fill_manual(values = c("green3", "green3", "green3","yellow1", "blue3", "blue3", "blue3", "blue3","red3", "red3", "red3")) + geom_bar(stat = "identity", colour = "black", aes(fill = variable)) + ylab("Probability") + xlab("") + ggtitle("Pitch Outcome Distribution - Lefty Hitters")
-    }
-  }
   
   ########################################################  
   
   
   ## Plot output
   output$pitch_plot <- renderPlot({
-    the.big.guy.R.pitcher(v$data, input$pitcher_name, input$other_pitcher)
+    the.big.guy.pitcher(v$data, input$pitcher_name, input$other_pitcher, "R")
   })
   
   output$pitch_plot_2 <- renderPlot({
-    the.big.guy.L.pitcher(v$data, input$pitcher_name, input$other_pitcher)
+    the.big.guy.pitcher(v$data, input$pitcher_name, input$other_pitcher, "L")
   })
   
   
